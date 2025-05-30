7d2308b7d1843dd26622de890494f0dca21c349b
hopedeveloper08 <hopedeveloper08@gmail.com>
2025-05-30 16:53:10 +0330

    llm module

--- src/rag_system/__init__.py
+++ src/rag_system/__init__.py
@@ -3,4 +3,4 @@ from src.rag_system.chunking import chunking_text
 from src.rag_system.embedding import load_embedding_model
 from src.rag_system.vectorstore import create_vector_store, load_vector_store
 from src.rag_system.retriever import get_retriver
-from src.rag_system.generation import get_prompt_template
+from src.rag_system.generation import get_prompt_template, get_llm
--- src/rag_system/generation.py
+++ src/rag_system/generation.py
@@ -1,5 +1,9 @@
 from langchain_core.prompts import ChatPromptTemplate
 
+from langchain_ollama import ChatOllama
+
+from src.utils import load_yaml
+
 
 def get_prompt_template(prompt_path):
     with open(prompt_path, 'r', encoding='utf-8') as f:
@@ -10,5 +14,9 @@ def get_prompt_template(prompt_path):
     ])
 
 
-def generator():
-    pass
+def get_llm(config_path):
+    config = load_yaml(config_path)
+    return ChatOllama(
+        model=config['model'],
+        temperature=config['temperature']
+    )
